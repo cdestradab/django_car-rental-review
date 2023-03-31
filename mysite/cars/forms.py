619b87d94ddfddecaecf5387b2e77d7bf7a51b73
cdestradab <cdestradab@gmail.com>
2023-03-30 21:43:29 -0500

    first changes

---
+++ mysite/cars/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ReviewForm(forms.Form):
+    first_name = forms.CharField(label='First Name', max_length=100)
+    last_name = forms.CharField(label='Last Name', max_length=100)
+    email = forms.EmailField(label='E-Mail')
+    review = forms.CharField(label='Ingrese su comentario aquí')
+ 
