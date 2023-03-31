5e66451c7083427336d13e768152fbbb4fc859c3
cdestradab <cdestradab@gmail.com>
2023-03-31 01:55:27 -0500

    widgets and styling DONE

--- mysite/cars/forms.py
+++ mysite/cars/forms.py
@@ -4,5 +4,8 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=100)
     last_name = forms.CharField(label='Last Name', max_length=100)
     email = forms.EmailField(label='E-Mail')
-    review = forms.CharField(label='Ingrese su comentario aquí')
+    review = forms.CharField(label='Ingrese su comentario aquí', 
+                             widget=forms.Textarea(attrs={'class':'myform', 
+                                                          'rows':'2',
+                                                          'cols':'5'}))
  
