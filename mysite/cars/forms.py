fd22b687c2da385e4510270a2b8e495a79e7775a
cdestradab <cdestradab@gmail.com>
2023-03-31 23:12:43 -0500

    ModelForms DONE

--- mysite/cars/forms.py
+++ mysite/cars/forms.py
@@ -1,11 +1,18 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100)
-    last_name = forms.CharField(label='Last Name', max_length=100)
-    email = forms.EmailField(label='E-Mail')
-    review = forms.CharField(label='Ingrese su comentario aquí', 
-                             widget=forms.Textarea(attrs={'class':'myform', 
-                                                          'rows':'2',
-                                                          'cols':'5'}))
+#Modelo de forma manual:
+#class ReviewForm(forms.Form):
+#    first_name = forms.CharField(label='First Name', max_length=100)
+#    last_name = forms.CharField(label='Last Name', max_length=100)
+#    email = forms.EmailField(label='E-Mail')
+#    review = forms.CharField(label='Ingrese su comentario aquí', 
+#                             widget=forms.Textarea(attrs={'class':'myform', 
+#                                                          'rows':'2',
+#                                                          'cols':'5'}))
  
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['first_name', 'last_name', 'stars']
--- mysite/cars/views.py
+++ mysite/cars/views.py
@@ -9,12 +9,15 @@ def rental_review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            #print(form.cleaned_data) --- Este pedazo solo mostraba los valores ingresados en la terminal
+            form.save()
             return redirect(reverse('cars:thank_you'))
+        
     #ELSE, RENDER FORM
     else:
         form = ReviewForm()
 
     return render(request, 'cars/rental_review.html', context={'form':form})
+
 def thank_you(request):
     return render(request, 'cars/thank_you.html')
