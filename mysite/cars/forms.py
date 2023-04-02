336a5b90986ce67160e4ccbdb9fd1b80babe07eb
cdestradab <cdestradab@gmail.com>
2023-04-02 01:36:51 -0500

    ModelForms Customizations DONE

--- mysite/cars/forms.py
+++ mysite/cars/forms.py
@@ -2,17 +2,21 @@ from django import forms
 from .models import Review
 from django.forms import ModelForm
 
-#Modelo de forma manual:
-#class ReviewForm(forms.Form):
-#    first_name = forms.CharField(label='First Name', max_length=100)
-#    last_name = forms.CharField(label='Last Name', max_length=100)
-#    email = forms.EmailField(label='E-Mail')
-#    review = forms.CharField(label='Ingrese su comentario aquí', 
-#                             widget=forms.Textarea(attrs={'class':'myform', 
-#                                                          'rows':'2',
-#                                                          'cols':'5'}))
  
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
-        fields = ['first_name', 'last_name', 'stars']
+        fields = "__all__" #Pasar todos los campos del modelo como campos del formulario.
+
+        labels = {
+            'first_name':"TU PRIMER NOMBRE",
+            'last_name':"TU APELLIDO",
+            'stars':"PUNTUACIÓN"
+        }
+
+        error_messages = {
+            'stars':{
+                'min_value': "El valor no puede ser menor a 0",
+                'max_value': "El valor máximo no puede ser mayor a 5"
+            }
+        }
